6cd155e0b799d48db2da0905a378cdf45fd49f59
Shavi Silva <shavinda34@gmail.com>
2022-05-20 23:33:37 +0930

    Setting up dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  get '/dashboard', to: 'dashboard#index'
+  get '/user/:id', to: 'user#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
